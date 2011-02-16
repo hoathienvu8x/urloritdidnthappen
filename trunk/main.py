e9c34583596606bdb0cdf319a490d6d70959d4db
elsigh@google.com <elsigh@google.com@8cddcd98-8853-b9af-3a92-2c6fd3c34de4>
2011-02-16 23:22:53 +0000

    user prototypes list /mine page + cleanups

--- trunk/main.py
+++ trunk/main.py
@@ -39,6 +39,7 @@ from django.utils import simplejson
 #### DB MODELS ####
 class Prototype(db.Model):
   user = db.UserProperty(required=True)
+  name = db.StringProperty()
   content = db.TextProperty(required=True)
   created = db.DateTimeProperty(auto_now_add=True)
   modified = db.DateTimeProperty(auto_now=True)
@@ -60,13 +61,23 @@ class RequestHandler(webapp.RequestHandler):
     server_software = os.getenv('SERVER_SOFTWARE')
     if server_software and 'Dev' in server_software:
       current_version_id = str(datetime.datetime.now())
-      is_production = True
+      is_production = False
+
     template_vars['is_production'] = is_production
     template_vars['current_version_id'] = current_version_id
+    template_vars['user'] = users.get_current_user()
     template_vars['login_url'] = users.create_login_url('/')
     template_vars['logout_url'] = users.create_logout_url('/')
     self.response.out.write(template.render('templates/%s' % template_name,
                                             template_vars))
+
+def FetchPrototype(key):
+  key = str(key)
+  if len(key) > 30:
+    return db.get(key)
+  else:
+    return db.get(db.Key.from_path('Prototype', int(key)))
+
 
 #### HANDLERS ####
 
@@ -85,13 +96,15 @@ class EditorHandler(RequestHandler):
       content = result.content
     else:
       try:
-        key = int(self.request.path[1:])
-        prototype = db.get(db.Key.from_path('Prototype', int(key)))
+        key = self.request.path[1:]
+        prototype = FetchPrototype(key)
+        logging.info('got prototype %s, %s' % (prototype, prototype.content))
         if prototype.user != user:
           # unsets the key so that any save action creates a new entity.
           key = ''
         content = prototype.content
       except ValueError:
+        logging.info('Got ValueError with key: %s' % key)
         content = ''
 
     template_vars = {
@@ -123,7 +136,7 @@ class SaveHandler(RequestHandler):
       return self.response.out.write('Cowardly refusal to save any content.')
 
     if key:
-      prototype = db.get(db.Key.from_path('Prototype', key))
+      prototype = FetchPrototype(key)
       if prototype.user != user:
         self.error(503)
         return self.response.out.write('You do not own this prototype.')
@@ -132,11 +145,26 @@ class SaveHandler(RequestHandler):
       prototype = Prototype(content=content, user=user)
     prototype.put()
 
-    self.redirect('/%s' % prototype.key().id())
+    self.redirect('/%s' % str(prototype.key()))
 
+
+class MineHandler(RequestHandler):
+  """Renders a user's prototypes."""
+  @login_required
+  def get(self):
+    user = self.request.get('user', users.get_current_user())
+    prototypes = db.Query(Prototype)
+    prototypes.filter('user =', user)
+    prototypes.order('-created')
+    template_vars = {
+      'pagename': 'mine',
+      'prototypes': prototypes
+    }
+    self.RenderTemplateOut('mine.html', template_vars)
 
 
 class RenderHandler(RequestHandler):
+  """Renders the raw html straight away, sans editor."""
   @login_required
   def get(self):
     try:
@@ -146,7 +174,7 @@ class RenderHandler(RequestHandler):
       self.error(404)
       return self.response.out.write('No prototype for your key')
     user = users.get_current_user()
-    prototype = db.get(db.Key.from_path('Prototype', key))
+    prototype = FetchPrototype(key)
     if not prototype:
       self.error(404)
       return self.response.out.write('No prototype exists with key: %s' % key)
@@ -157,6 +185,7 @@ def main():
   application = webapp.WSGIApplication(
                                        [(r'/save/.*', SaveHandler),
                                         (r'/render/.*', RenderHandler),
+                                        (r'/mine', MineHandler),
                                         (r'/.*', EditorHandler),],
                                        debug=True)
   wsgiref.handlers.CGIHandler().run(application)
