ff52fdc587346358fe8b5395f6fb63b612b8e84e
mikol <mikol@google.com>
2011-02-16 19:50:23 +0000

    Add makefile and dependencies.

---
+++ static/require.js
@@ -0,0 +1,11 @@
+goog.require("goog.array");
+goog.require("goog.dom");
+goog.require("goog.dom.classes");
+goog.require("goog.dom.selection");
+goog.require("goog.events");
+goog.require("goog.events.KeyCodes");
+goog.require("goog.events.KeyHandler");
+goog.require("goog.net.XhrIo");
+goog.require("goog.style");
+goog.require("goog.Uri");
+goog.require("goog.Uri.QueryData");
