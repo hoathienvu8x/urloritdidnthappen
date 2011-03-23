53892e64dae6a742b1db066bed86eee93594cefd
elsigh@google.com <elsigh@google.com@8cddcd98-8853-b9af-3a92-2c6fd3c34de4>
2011-03-23 18:33:35 +0000

    new compile script, autosave

---
+++ trunk/static/compile_js.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Compiled for dev
+../deps/closure-library/closure/bin/calcdeps.py -i editor.js -p ../deps/closure-library/ -o list | grep closure | xargs cat > dev.js
+
+# Compiled for production
+../deps/closure-library/closure/bin/calcdeps.py -i editor.js -p ../deps/closure-library/ -o compiled -f "--compilation_level=ADVANCED_OPTIMIZATIONS" -c ../deps/closure-compiler/build/compiler.jar  > editor.prod.js;
