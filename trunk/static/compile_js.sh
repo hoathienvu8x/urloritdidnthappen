ccfd634f7c20d68d846fc25a16911fea2a71f97c
elsigh@google.com <elsigh@google.com@8cddcd98-8853-b9af-3a92-2c6fd3c34de4>
2011-03-23 21:24:29 +0000

    use the local jar to compile

--- trunk/static/compile_js.sh
+++ trunk/static/compile_js.sh
@@ -4,4 +4,4 @@
 ../deps/closure-library/closure/bin/calcdeps.py -i editor.js -p ../deps/closure-library/ -o list | grep closure | xargs cat > dev.js
 
 # Compiled for production
-../deps/closure-library/closure/bin/calcdeps.py -i editor.js -p ../deps/closure-library/ -o compiled -f "--compilation_level=ADVANCED_OPTIMIZATIONS" -c ../deps/closure-compiler/build/compiler.jar  > editor.prod.js;
+../deps/closure-library/closure/bin/calcdeps.py -i editor.js -p ../deps/closure-library/ -o compiled -f "--compilation_level=ADVANCED_OPTIMIZATIONS" -c ../deps/jar/compiler.jar  > editor.prod.js;
